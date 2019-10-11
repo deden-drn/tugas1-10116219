bebd7d53947adebf233ac8ccf5815dd1840a4a7e
deden <deden@mahasiswa.unikom.ac.id>
2019-10-12 00:35:53 +0700

    penambahan index

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+  <p> Nim : 10116219 <br>
+      Nama : Deden Rizki Nuryana </p>
+      <a href="https://tugas1-master-cc4.herokuapp.com" target="_blank"> Kembali ke Master </a>
+</body>
+</html>
