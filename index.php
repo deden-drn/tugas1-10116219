0567f820c66f459d719b89f41328ee382fa6ff08
deden-drn <56196102+deden-drn@users.noreply.github.com>
2019-10-12 00:54:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,71 @@
-<html>
-<body>
-  <p> Nim : 10116219 <br>
-      Nama : Deden Rizki Nuryanaaaaaaaaaaaaaaaaaaaaaaa  </p>
-      <a href="https://tugas1-master-cc4.herokuapp.com" target="_blank"> Kembali ke Master </a>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TUGAS1-101162xx-10</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: black;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .links > a {
+                color: black;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div id="iniglobe"></div>
+        <div class="flex-center position-ref full-height">
+            
+            <div class="content">
+
+                <div class="links">
+                    <b>10116219 - Deden Rizki Nuryana</b>
+                </div>
+
+                <br>
+
+                <div class="links">
+                    <a href="https://tugas1-master-cc4.herokuapp.com" target="_blank">Kembali - MASTER</a>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
